3a991a045dbe7b92ece91b2144900fdff566a14d
arzu <fatiharzu1@gmail.com>
2023-06-18 16:30:38 +0200

    18-06-23

---
+++ src/components/01-hello-world/hello-world.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import HelloReact from '../02-hello-react/hello-react'
+
+const HelloWorld = () => {
+  return (
+    <div><em>Hello</em><b>World</b>
+    <HelloReact/>
+    </div>
+  )
+}
+
+export default HelloWorld
---
+++ src/components/03-jsx/jsx1.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Jsx1 = () => {
+  return (
+    <div>
+        <div>Jsx1  Tüm elementler tek bir root icinde olmalidir.</div>
+        <div>Elementlerin attribute leri camelCase olmalidir</div>
+        <div>class isimleri className= ' ' ile verilir</div>
+        <div className='title'>Merhaba Jsx</div>
+    </div>
+  )
+}
+
+export default Jsx1
